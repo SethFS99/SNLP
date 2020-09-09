0410a6cc61e4cffc66b2fb5dd325ea1ca1efd250
Jake Lasley <jlasley@miners.utep.edu>
2020-09-09 14:43:05 -0600

    Add build-model.py which calculates weights
    Update README to explian use of build-model.py
    Add surname-dev.csv for training models

--- README.md
+++ README.md
@@ -1,3 +1,12 @@
 # SNLP
 All of my speech and natural language processing code
 This repository will be holding the project for identifying the nationality of someone based on the names
+
+# build-model.py
+This script takes in a set of training data with lines of the form
+
+    surname,nationality
+
+as in surnames-dev.csv.
+It outputs a vector of weights produced by multiple linear regression to a .npy file.
+This file of weights can then be read into another script for use in classification of surnames.
---
+++ build-model.py
@@ -0,0 +1,61 @@
+#!/usr/bin/env python3
+
+# build-model.py
+# Using multiple linear regression to
+# generate weights for classifying surnames
+# September 2020
+# Seth Flores and Jake Lasley
+
+import numpy as np
+from string import ascii_lowercase
+import sys
+
+def getUnigramVector(word):
+    word = word.lower()
+    uv = { s:0 for s in ascii_lowercase }
+    for letter in word:
+        try:
+            uv[letter] += 1
+        except KeyError:
+            print('KeyError when processing: ', letter, ' ... ignoring', file=sys.stderr)
+            continue
+    normalized =  [ count/len(word) for count in list(uv.values()) ] 
+    return normalized
+
+if __name__ == "__main__":
+    if len(sys.argv) != 3:
+        print("Usage: python build-model.py " +
+              "<input file>  <Nationality>" )
+        sys.exit()
+
+
+    # Turning names into vectors
+    surname_vectors = []
+    class_ = []
+    with open(sys.argv[1], mode="r", encoding="utf-8") as input_file:
+        target = sys.argv[2]
+        # Create 2d list of surname unigram vectors
+        # and 2d list of classifications for those vectors
+        for line in input_file:
+            temp = line.strip().split(",")
+            surname = temp[0]
+            nationality = temp[1]
+            surname_vectors.append(getUnigramVector(surname))
+            if nationality == target:
+                class_.append(1)         # 1 ==> Russian
+            else:
+                class_.append(0)
+
+    # Linear Regression
+    # Convert lists to np.matrix like
+    surname_mat = np.array(surname_vectors)
+    class_mat = np.array(class_)
+
+    weights, residuals, rank, s = np.linalg.lstsq(surname_mat, class_mat, rcond=None)
+    print(np.shape(weights))
+    fname = target.lower() + "-weights"
+    np.save(fname, weights)
+    print("Weights saved to ", fname, ".npy")
+                
+            
+
