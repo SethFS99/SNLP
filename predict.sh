146744aeb119b99103bd1c7a5c11e30aa9659638
Jake Lasley <jlasley@miners.utep.edu>
2020-09-16 23:32:16 -0600

    Fix predict.sh so it doesnt overwrite the bigram results

--- predict.sh
+++ predict.sh
@@ -3,4 +3,4 @@
 LANG="English"
 
 python predict.py $LANG-bigram-conf.csv $LANG-bigram-stats.csv $LANG 0.{1..9} --multi
-python predict.py $LANG-unigram-conf.csv $LANG-bigram-stats.csv $LANG 0.{1..9} --multi
+python predict.py $LANG-unigram-conf.csv $LANG-unigram-stats.csv $LANG 0.{1..9} --multi
